4f42e261b40b8b23b6cc95da6761a023afd32736
mirgags <markmiraglia@gmail.com>
2013-07-30 16:29:18 -0700

    finished chap 7 skipped testing exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
 end
